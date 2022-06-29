895a0ab7b45798caea7f69914694644ffca5a721
KauanR <kau.matosr@gmail.com>
2022-06-29 16:58:58 -0300

    fix: info component update bug

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -1,17 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, Input, OnChanges } from '@angular/core'
 
 @Component({
     selector: 'app-info',
     templateUrl: 'info.component.html',
     styleUrls: ['info.component.scss']
 })
-export class InfoComponent implements OnInit {
+export class InfoComponent implements OnChanges {
 
     @Input() entries: number[] = []
 
     entriesChunks: Array<number[]> = []
 
-    ngOnInit(): void {
+    ngOnChanges(): void {
         for(let i = 0; i < this.entries.length; i+= 10)
             this.entriesChunks.push(this.entries.slice(i, i + 10))
 
