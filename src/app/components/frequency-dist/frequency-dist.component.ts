5746fbbffea873ee3751eaf5df899fc147e99a02
KauanR <kau.matosr@gmail.com>
2022-06-29 16:26:55 -0300

    feat: frequency-dist component implementation

--- src/app/components/frequency-dist/frequency-dist.component.ts
+++ src/app/components/frequency-dist/frequency-dist.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnChanges } from '@angular/core'
+import * as ss from 'simple-statistics'
+import { FrequencyDistRow } from './interfaces/fd-row'
 
 @Component({
     selector: 'app-frequency-dist',
@@ -9,8 +11,47 @@ export class FrequencyDistComponent implements OnChanges {
 
     @Input() entries: number[] = []
 
+    table: FrequencyDistRow[] = []
+    xiFiSum: number = 0
+    facSum: number = 0
+
     ngOnChanges(): void {
-        console.log(this.entries)
+        this.table = []
+        this.xiFiSum = 0
+        this.facSum = 0
+
+        const R = ss.max(this.entries) - ss.min(this.entries)
+        const K = Math.round(1 + 3.22 * Math.log10(this.entries.length))
+        const H = parseInt((R / K).toString()) + 1
+
+        let aux = ss.min(this.entries)
+
+        for(let i = 0; i < K; i++) {
+            const min = Math.round(aux)
+            const max = Math.round(aux + H)
+
+            const fi = this.entries.reduce((acc, cur) => {
+                if(cur >= min && cur < max) acc++
+                return acc
+            }, 0)
+
+            const xi = (min + max) / 2
+
+            this.table.push({
+                name: `${min} ├ ${max}`,
+                fi,
+                xi,
+                fac: this.facSum,
+                fiPercent: (fi * 100) / this.entries.length,
+                xiFi: xi * fi
+            })
+
+            aux += H
+            this.xiFiSum += xi * fi
+            this.facSum += fi
+        }
+
+        console.log('Distribuição de Frequências', this.table)
     }
 
 }
---
+++ src/app/components/frequency-dist/interfaces/fd-row.ts
@@ -0,0 +1,8 @@
+export interface FrequencyDistRow {
+    name: string
+    fi: number
+    xi: number
+    fac: number
+    fiPercent: number
+    xiFi: number
+}
